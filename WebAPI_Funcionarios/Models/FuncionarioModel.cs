777f49583a6cc18e929f91882b0b336bece00a55
Murillo Santos <murillo.lds01@gmail.com>
2024-09-18 14:43:58 -0300

    chore: minor formatting and code cleanup.

--- WebAPI_Funcionarios/Models/FuncionarioModel.cs
+++ WebAPI_Funcionarios/Models/FuncionarioModel.cs
@@ -1,9 +1,11 @@
-﻿using WebAPI_Funcionarios.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using WebAPI_Funcionarios.Enums;
 
 namespace WebAPI_Funcionarios.Models
 {
     public class FuncionarioModel
     {
+        [Key]  
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
--- WebAPI_Funcionarios/Models/ServiceResponse.cs
+++ WebAPI_Funcionarios/Models/ServiceResponse.cs
@@ -5,6 +5,5 @@
         public T? Dados { get; set; }
         public string Mensagem { get; set; } = string.Empty;
         public bool Sucesso { get; set; } = true;
-
     }
 }
