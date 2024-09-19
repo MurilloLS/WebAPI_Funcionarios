62f0ce99136ea27b838a3fd3148875284fdce241
Murillo Santos <murillo.lds01@gmail.com>
2024-09-19 14:16:29 -0300

    feat: add initial structure for FuncionarioService.

---
+++ WebAPI_Funcionarios/Service/FuncionarioService/FuncionarioService.cs
@@ -0,0 +1,44 @@
+﻿using WebAPI_Funcionarios.DataContext;
+using WebAPI_Funcionarios.Models;
+
+namespace WebAPI_Funcionarios.Service.FuncionarioService
+{
+    public class FuncionarioService : IFuncionarioInterface
+    {
+        private readonly ApplicationDbContext _context;
+        public FuncionarioService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> CreateFuncionario(FuncionarioModel newFuncionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> DeleteFuncionario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<FuncionarioModel>> GetFuncionarioById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> GetFuncionarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> InativaFuncionario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> UpdateFuncionario(FuncionarioModel editedFuncionario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
