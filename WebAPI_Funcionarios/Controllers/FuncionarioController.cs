2229819fb35a219b14da9341aa2dc756d47ea229
Murillo Santos <murillo.lds01@gmail.com>
2024-09-19 16:09:04 -0300

    feat: implement GET endpoint for retrieving funcionarios.

--- WebAPI_Funcionarios/Controllers/FuncionarioController.cs
+++ WebAPI_Funcionarios/Controllers/FuncionarioController.cs
@@ -15,8 +15,11 @@ namespace WebAPI_Funcionarios.Controllers
             _funcionarioInterface = funcionarioInterface;
         }
 
-
-
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> GetFuncionarios()
+        {
+            return Ok(await _funcionarioInterface.GetFuncionarios());
+        }
 
     }
 }
