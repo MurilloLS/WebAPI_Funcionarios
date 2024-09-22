39001c4cc0af055db263a5fbe710f452061f8353
Murillo Santos <murillo.lds01@gmail.com>
2024-09-21 22:34:51 -0300

    fix: resolve tracking issue in Enums using JsonConverter.

--- WebAPI_Funcionarios/Enums/TurnoEnum.cs
+++ WebAPI_Funcionarios/Enums/TurnoEnum.cs
@@ -1,5 +1,8 @@
-﻿namespace WebAPI_Funcionarios.Enums
+﻿using System.Text.Json.Serialization;
+
+namespace WebAPI_Funcionarios.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum TurnoEnum
     {
       Manha,
