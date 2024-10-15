ee3efb80080a4d738b5f1faac28fdc3f771c1f45
Murillo Santos <murillo.lds01@gmail.com>
2024-10-14 21:39:28 -0300

    feat(controller): add ContaController with JWT authentication

---
+++ WebAPI_Funcionarios/Controllers/ContaController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebAPI_Funcionarios.Models;
+
+namespace WebAPI_Funcionarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContaController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginModel login)
+        {
+            if(login.Login == "admin" && login.Password == "admin")
+            {
+                var token = GenerateTokenJWT();
+                return Ok(new {token});
+            }
+            return BadRequest(new { message = "Credenciais inválidas. Por favor, verifique seu nome de usuario e senha." });
+
+        }
+
+
+        private string GenerateTokenJWT()
+        {
+            string secretKey = "04c65134-de6f-4e21-bdad-a7bcd5e6ebbf";
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim("login", "admin"),
+                new Claim("nome", "Administrador do Sistema"),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "sua_empresa",
+                audience: "sua_aplicacao",
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }   
+}
