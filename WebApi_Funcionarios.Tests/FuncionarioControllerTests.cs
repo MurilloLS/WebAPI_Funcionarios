1d4e7199e1920916068463003d994b4ade3ef12f
Murillo Santos <murillo.lds01@gmail.com>
2024-10-21 15:33:03 -0300

    test(funcionario): add test for GetFuncionarios method

--- WebApi_Funcionarios.Tests/FuncionarioControllerTests.cs
+++ WebApi_Funcionarios.Tests/FuncionarioControllerTests.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using WebAPI_Funcionarios.Controllers;
+using WebAPI_Funcionarios.Models;
 using WebAPI_Funcionarios.Service.FuncionarioService;
 
 namespace WebApi_Funcionarios.Tests;
@@ -17,5 +19,27 @@ public class FuncionarioControllerTests
 
         // Assert
         Assert.NotNull(controller);
+    }
+
+    [Fact]
+    public async Task GetFuncionarios_ReturnsOkResult_WhenCalled()
+    {
+        // Arrange
+        var mockService = new Mock<IFuncionarioInterface>();
+        mockService.Setup(service => service.GetFuncionario())
+            .ReturnsAsync(new ServiceResponse<List<FuncionarioModel>>
+            {
+                Sucesso = true,
+                Dados = new List<FuncionarioModel>()
+            });
+        var controller = new FuncionarioController(mockService.Object);
+
+        // Act
+        var result = await controller.GetFuncionarios();
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var serviceResponse = Assert.IsType<ServiceResponse<List<FuncionarioModel>>>(okResult.Value);
+        Assert.True(serviceResponse.Sucesso);
     }
 }
